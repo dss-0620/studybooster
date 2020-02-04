18759f42d0249abd9a8e8e3a76db2c0908aff0a6
Devansh <devanshsuthar0612@gmail.com>
2020-02-04 09:29:53 +0530

    Still Some Bug needs to fixed!

--- user/features/add_result/index.php
+++ user/features/add_result/index.php
@@ -1,36 +1,5 @@
 <?php include "../header.php"; ?>
-<body>
-    <header>
-            <div class="container-fluid px-0">
-                    <div class="navbar bg-dark navbar-dark navbar-expand-sm">
-                        <h2 class="navbar-label d-none d-sm-inline-block m-0 p-0" style="color:white; font-weight:100;">User</h2>
-                        <div class="navbar-nav mr-5 ml-sm-auto">
-                            <a href="../../../" class="nav-item nav-link mr-3"><i class="fas fa-home mr-1"></i>Home</a>
-                            <div class="dropdown">
-                              <a href="#" class="nav-item nav-link dropdown-toggle" id="user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="icon ion-md-person mr-1"></i><?php echo $_SESSION['firstname'];?></a>
-                               <div class="dropdown-menu" aria-labelledby="user">
-                                  <a href="../profile" class="dropdown-item">Profile</a>
-                                  <a href="../logout.php" class="dropdown-item">Log Out</a>
-                               </div>  
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row pt-4 main-cont-space1">
-                    <div class="col-md-2 mt-3">
-                            <div class="row pl-3 mb-3 link-type1"><a href="../../"><i class="fas fa-tachometer-alt mr-2"></i>Dashboard</a></div>
-                            <div class="row pl-3 my-3 link-type1"><a href="../add_result"><i class="far fa-plus-square mr-2"></i>Create new result</a></div>
-                            <div class="row pl-3 my-3 link-type1"><a href="../grade_result"><i class="fas fa-calculator mr-2"></i>Calculate S.P.I. by Grades</a></div>
-                            <div class="row pl-3 my-3 link-type1"><a href="../view_saved_results"><i class="fas fa-binoculars mr-2"></i>View saved results</a></div>
-                            <div class="row pl-3 my-3 link-type1"><a href="../change_password"><i class="far fa-edit mr-2"></i>Change Password</a></div>
-                            <div class="row pl-3 my-3 link-type1"><a href="../feedback"><i class="far fa-comments mr-2"></i>Give feedback</a></div>
-                    </div>
-                    <div class="col-md-9 main-content1 ">
-                        <div class="headings-cont1">
-                        <h2  class="heading-main1">Welcome,</h2> 
-                        <h4  class="heading-user1" style="font-weight:500; font-size:130%; color:grey;"><?php echo $_SESSION['firstname'];?>&nbsp;<?php echo $_SESSION['lastname'];?><h4>
-                        <!--<div class="alert alert-primary mt-3" style="font-size:18px;">-->
-                        <!--        For <strong>calculating S.P.I. by grades </strong><a href='../grade_result'>Click Here</a>.-->
-                        <!--    </div>-->
+<?php include "../navigation.php"; ?>
                     <?php if($_SESSION['user_semester_id'] == 1 && !isset($_GET['show_spi'])){?>
                     <div class="row mx-2 mx-md-5 my-5 p-2" style="font-weight:350; font-size:87%; border:1px solid black;"> 
                          <div class="col-11" style="text-align:center;">
@@ -438,7 +407,27 @@
        .link-type1 a:hover {
            color: blue; text-decoration: none;
        }
-
+       .mobile-icon{
+           font-size:18px;
+           background-color:#f1f3f6;
+           display:none;
+           color:black;
+       }
+       .mobile-icon i{
+           font-size:20px;
+           margin-left:30px;
+           margin-top:10px;
+           cursor:pointer;
+       }
+        @media only screen and (max-width:767px)
+       {
+           .main-menu1{
+               display:none;
+           }
+           .mobile-icon{
+               display:block;
+           }
+       }
        .main-content1 {
           background-color:#fff; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3); border-radius: 5px; margin-bottom: 2.5vh;
        }
