e6f3e665d918a2990abe90f1b85a6c3ec856294f
Devansh <devanshsuthar0612@gmail.com>
2020-02-05 17:27:11 +0530

    New and Better changes in original layout using new Ui

--- index.php
+++ index.php
@@ -113,46 +113,39 @@
            </div> -->
        </div>
 
-       <!--Main content of header-->
-       <div class="header-main-content1">
-           <h1 class="main-heading1 mb-3">Enhance Your Studies with studybooster</h1>
-          <?php
-          if(!isset($_SESSION['username'])){
-          echo '<div class="anim"><a href="user/features/signup.php" class="heading-button1" style="text-decoration:none;">Get started</a></div>';
-          }
-         else if($_SESSION['role'] == 'admin'){ 
-          echo '<div class="anim"><a href="admin/" class="heading-button1" style="text-decoration:none;">Admin Page</a></div>';
-           }
-         else if($_SESSION['role'] == 'user'){ 
-          echo '<div class="anim"><a href="user/" class="heading-button1" style="text-decoration:none;">User Page</a></div>';
-         }
-         ?>
-       </div>
-
-       <!--Login form in header-->
-       <!-- <form method="POST" action="php/login.php" class="login-form">
-            <div class="row1" id="underline1">
-                <p id="p1">Log In to your account</p>
-                <a href="index.php?action=close_login"><i class="icon ion-md-close icn1"></i></a>
+       <!--Main content of header along with login-->
+       <div class="container" style="padding-top:170px;">
+            <div class="row">
+                <div class="col-md-6 pt-5">
+                    <h2 class="text-light main-title mb-4">Enhance Your Studies With Studybooster</h2>
+                    <a href="php/login_page.php" class="heading-button1 mr-2">Log In</a>
+                    <a href="user/features/signup.php" class="heading-button2 ml-2">Sign Up</a>
+                </div>
+                <div class="col-md-6  d-none d-md-flex justify-content-md-end">
+                    <div class="card text-center text-light col-md-12 col-lg-10" style="background-color:rgb(252,252,252);">
+                        <div class="card-body">
+                            <h5 class="card-text mt-3 mb-5 text-dark">Log In</h3>
+                            <form action="php/login.php" class="form-group" method="POST">
+                                <div class="form-group input-group my-4">
+                                        <div class="input-group-prepend"><i class="icon ion-md-person input-group-text" style="font-size:1.2rem;"></i></div>
+                                    <input type="text" name="username" class="form-control" placeholder="Username / E-mail">
+                                </div>
+                                <div class="form-group input-group my-4">
+                                        <div class="input-group-prepend"><i class="icon ion-md-lock input-group-text" style="font-size:1.3rem;"></i></div>
+                                    <input type="password" name="password" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group mt-4 mb-4">
+                                    <input type="submit" value="Login Securely" class="btn btn-primary btn-block">
+                                </div>
+                                <div class="form-group mt-4" style="font-size:82%;">
+                                    <a href="forgot_password/">Forgot Password?</a> 
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="wrong1">
-                <p class="sign">Your E-mail or password is incorrect</p>
-            </div>
-            <div class="row1">
-                <input name="username" id="username" type="email" value="<?php echo $x; ?>" placeholder="E-mail" required>
-            </div>
-            <div class="row1">
-                <input name="password" id="password" type="password" placeholder="Password" required>
-            </div>
-            <div class="row1 row2 d-flex align-items-center">
-            <small><a href="forgot_password">Forgot password?</a></small> 
-            <input name="submit" value="Log In" type="submit" class="btn btn-primary mt-0" style="width:auto; margin-left:33%;">
-            </div>
-            <div class="row1">
-                <small id="sp">Don't have an account?</small>
-                <small><a href="user/php/signup.php">Create now</a></small>
-            </div>
-           </form> -->
+        </div>
     </div> 
          <style>
             html{
@@ -169,58 +162,11 @@
                 .wrong1{
                     font-size:86%;
                 }
-                @media only screen and (max-width:767px)
-                {
-                    .mobile-icon{
-                        display: inline-block;
-                        color:white;
-                    }
-                    .navi{
-                        display:none;
-                    }
-                    .but1:hover,.but1:active{
-                        color:#0b0;
-                        border:none;
-                    }
+                .main-title{
+                    font-size:205%;
+                    color:#d5d5d5 !important;
                 }
-                /*@media only screen and (max-width:400px)*/
-                /*{*/
-                /*    .login-form*/
-                /*    {*/
-                /*        width:97%;*/
-                /*    }*/
-                /*    .main-heading1{*/
-                /*        font-size: 150% !important;*/
-                /*    }*/
-                /*    .anim{*/
-                /*        font-size: 50% !important;*/
-                /*    }*/
-                /*}*/
-                @media only screen and (max-width:500px)
-                {
-                    .login-form
-                    {
-                        width:97%;
-                    }
-                    .main-heading1{
-                        font-size: 155% !important;
-                    }
-                    .anim{
-                        font-size: 70% !important;
-                    }
-                    .logo{
-                        height:90px; width:auto; float:left; margin-top:6px;
-                    }
-                    .mobile-icon{
-                        font-size:150%;
-                    }
-                }
-                @media only screen and (max-width:400px)
-                {
-                    body{
-                        font-size:17px;
-                    }
-                }
+            
             </style>
     </header>
      <!----/****Features of webpage****/---->
